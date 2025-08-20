e07f0542efa4eacb3b63cd0ed237631aba9929de
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2025-08-20 16:23:59 +0100

    feat: added assets and dependencies

---
+++ lib/src/routing/router_config.dart
@@ -0,0 +1,15 @@
+enum AppRoute {
+  splashscreen,
+  onboarding,
+  bottomNavigation,
+  settings,
+}
+
+
+class RoutePath {
+  // unauthenticated routes
+  static const splash = '/splash';
+  static const onboarding = '/onboarding';
+  static const bottomNavigation = '/bottomNavigation';
+  static const settings = '/settings';
+}
